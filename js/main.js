e9af3f39f697ca67f8ae4172687e943d2b238a76
VictorDev09 <victorfrazao09@gmail.com>
2025-12-10 14:56:18 -0300

    continuacao

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
-import ui from "./ui.js"
+import ui from "./ui.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-  ui.renderizarPensamentos()
-})
+  ui.renderizarPensamentos();
+
+  const formularioPensamento = document.getElementById("pensamento-form");
+
+  formularioPensamento.addEventListener("submit", async (evento) => {
+    evento.preventDefault(); // Evita que a página recarregue sozinha
+
+    const conteudo = document.getElementById("pensamento-conteudo").value;
+    const autoria = document.getElementById("pensamento-autoria").value;
+
+    try {
+      await api.salvarPensamento({ conteudo, autoria });
+
+      window.location.reload();
+    } catch {
+      alert("Erro ao salvar o pensamento");
+    }
+  });
+});
--- js/ui.js
+++ js/ui.js
@@ -8,11 +8,19 @@ const ui = {
       const pensamentos = await api.buscarPensamentos()
       pensamentos.forEach(pensamento => {
         listaPensamentos.innerHTML += `
-          <li class="li-pensamento" data-id="${pensamento.id}">
-          <img src="assets/imagens/aspas-azuis.png" alt="Aspas azuis" class="icone-aspas">
-          <div class="pensamento-conteudo">${pensamento.conteudo}</div>
-          <div class="pensamento-autoria">${pensamento.autoria}</div>
-          </li>
+         <li class="li-pensamento" data-id="${pensamento.id}">
+  <img src="assets/imagens/aspas-azuis.png" alt="Aspas azuis" class="icone-aspas">
+  <div class="pensamento-conteudo">${pensamento.conteudo}</div>
+  <div class="pensamento-autoria">${pensamento.autoria}</div>
+  <div class="icones">
+    <button class="botao-editar">
+        <img src="assets/imagens/icone-editar.png" alt="Editar">
+    </button>
+    <button class="botao-excluir">
+        <img src="assets/imagens/icone-excluir.png" alt="Excluir">
+    </button>
+  </div>
+</li>
         `
       })
     }
